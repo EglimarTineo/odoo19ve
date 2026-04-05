b71726d4bec78d53be7dd735b489ef1447eaae25
EglimarTineo <eglimart22@gmail.com>
2026-04-05 17:33:40 -0400

    Create __manifest__.py

---
+++ l10n_ve_sumitic/__manifest__.py
@@ -0,0 +1,25 @@
+# Part of Sumitic. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Venezuela - Localización Sumitic",
+    "version": "19.0.1.0",
+    "category": "Accounting/Localizations/Account Charts",
+    "summary": "Localización contable estandarizada para Venezuela por Sumitic",
+    "author": "Sumitic",
+    "website": "https://sumitic.lat",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        "base_vat",
+        "purchase",
+        "sale",
+    ],
+    "data": [
+        "data/res_currency_data.xml",
+        "data/template/account.tax.group.csv",
+        "data/template/account.account.csv",
+        "data/template/account.tax.csv",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
